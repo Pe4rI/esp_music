2a44b04999a13320cac43289ddaab88daba407bd
Pe4rI <fil3keepr.shinju@protonmail.com>
2024-03-21 21:55:13 +0100

    added a proper note lenght calculation and fixed testing tune

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ A crappy code allowing you to play melody though buzzer.
 It is primarily meant for ESP32-WROOM-32E, but should work on other microcontrollers as well, but may require some modifications.
 
 ## Todo
-- [] Use universal duty cycle
-- [] Fix two consecutive notes sounding as one (small delay)
+- [ ] Use universal duty cycle
+- [X] Fix two consecutive notes sounding as one (small delay)
--- player.py
+++ player.py
@@ -7,30 +7,33 @@ from notes import Note
 from tunes import TEST
 
 # initial values
-pwm_pin_4 = PWM(Pin(33))
-pwm_pin_4.freq(4000)
-pwm_pin_4.duty(0)
+player_pin = PWM(Pin(33))
+player_pin.freq(4000)
+player_pin.duty(0)
 
 # it is necessary to add small intervals of silence,
 # so individual notes may be heard
-end_cut = 2     # ms
+end_cut = 10     # ms
 
 # play
 tempo = 120
+
+# calculates lenght of a single beat in miliseconds
+beat = 60000*4/tempo
 
 for i in TEST:
     # print('Frequency of ', i.freq(),
     # 'played for ', 1000/tempo*i.dur(),
     # 'ms with duty of ', i.dut())
     
-    pwm_pin_4.freq(i.freq())
-    pwm_pin_4.duty(i.dut())
-    time.sleep_ms(int(200000/tempo*i.dur() - end_cut)) ##200000 hardcoded for now
+    player_pin.freq(i.freq())
+    player_pin.duty(i.dut())
+    time.sleep_ms(int(beat*i.dur() - end_cut))
 
     # the silent part
-    pwm_pin_4.duty(0)
+    player_pin.duty(0)
     time.sleep_ms(end_cut)
     
 # volume 0
-pwm_pin_4.duty(0)
+player_pin.duty(0)
  
